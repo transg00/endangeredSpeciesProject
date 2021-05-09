6de14897ca25de5af4955f766d8ab8c6fd9ced45
transg00 <transg@mymail.vcu.edu>
2021-05-09 15:03:13 -0400

    testing

--- connection.php
+++ connection.php
@@ -21,6 +21,8 @@ try {
 // Start or resume session variables
 session_start();
 
+/*
+
 // If the user_ID session is not set, then the user has not logged in yet
 if (!isset($_SESSION['user_ID']))
 {
@@ -54,5 +56,6 @@ if (!isset($_SESSION['user_ID']))
         exit();
     }
 }
+*/
 
 ?>
