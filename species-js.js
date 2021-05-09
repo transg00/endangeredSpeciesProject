b57b573603dd14202efc9a4fdccb2dc63d476314
transg00 <transg@mymail.vcu.edu>
2021-05-09 15:54:24 -0400

    Update species-js.js

--- species-js.js
+++ species-js.js
@@ -1,28 +1,19 @@
 $(document).ready(function(){
 
-
-    $('#table-rest').DataTable({
-         "dom": 'Blfrtip',
-         "autoWidth": false,
-         "processing":true,
-         "serverSide":true,
-         "pageLength":15,
-         "lengthMenu":[[15, 25, 50, 100, -1], [15, 25, 50, 100, "All"]],
-         "processing": true,
-         "language": {processing: '<i class="fa fa-spinner fa-spin fa-2x fa-fw"></i>'},
-         "order":[],
-         "ajax":{
-             url:"species.php", 
-             type:"POST",
-             data:{
-                     action:'listSpecies'
+    $('#table-employee').DataTable({
+        "dom": 'Blfrtip',
+        "ordering":false,
+        "bLengthChange": false,
+        "searching": false,
+        "paging": false,
+        "ajax":{
+            url:"species.php",
+            type:"POST",
+            data:{
+                    action:'listSpecies'
                  },
-             dataType:"json"
-         },
-         "columnDefs":[ {"targets":[2], "visible":true} ],
+            dataType:"json"
+        }
+    });
 
-     });
- 
-
- });
- 
+});
